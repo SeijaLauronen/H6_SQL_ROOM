faa4dc45c290d94fa7825533a33860a20a957529
SeijaLauronen <seija.lauronen@gmail.com>
2020-02-18 12:09:24 +0200

    Tulostaa tietokannan sisällön aluksi näytölle tavan tekstiboksiin. DONE: Tulostus myös oikeaan listaan adapteria käyttäen. DONE: Tulosta myös tallennuksen jälkeen päivitetyn listan.Paitsi jotain höpöä tulee, ei vielä ok muuta kuin pvm.

--- app/src/main/java/com/example/ssl_sql_lite/MainActivity.java
+++ app/src/main/java/com/example/ssl_sql_lite/MainActivity.java
@@ -64,10 +64,7 @@ public class MainActivity extends AppCompatActivity {
         //tauluDao.InsertTaulu(t);
         this.tauluDao=tietokanta.tauluDao();
         tauluDao.getAllInDescenfingOrder();
-
         str= "";
-
-
         this.save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -79,160 +76,34 @@ public class MainActivity extends AppCompatActivity {
                 Calendar calendar = new GregorianCalendar();
                 t.pvm = calendar.getTime().toString();
                 tauluDao.InsertTaulu(t); //Kaatu null objektiin, taulu-dao on nulli.
-
+                ShowResults();
             }
+
         });
 
-        //Taulu tResult=new Taulu();
-        //ArrayAdapter<String> itemsAdapter =new ArrayAdapter<String>
-                //new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-
-
-
-        List<Taulu> tResults=tauluDao.getAllInDescenfingOrder();
-        adapteri = new Adapteri(this,R.layout.adapteri, tResults); //Mitähän tuohon viimeseen piti laittaa
-        listView.setAdapter(adapteri);
-        adapteri.addAll(tResults);
-/*
-        //TextView otsikko = findViewById(R.id.otsikko);
-        //TextView teksti = findViewById(R.id.textiosa);
-        Context c = null;
-        LayoutInflater layoutInflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View row = layoutInflater.inflate(R.layout.activity_main,R.layout.activity_main,false);
-        TextView otsikko = (TextView) row.findViewById(R.id.otsikko);
-        TextView teksti = (TextView) row.findViewById(R.id.textiosa);
-        SingleRow tmp = list.get(position);
-        otsikko.setText(tmp.Otsikko);
-        teksti.setText(tmp.Teksti);
-
-*/
-
-
-        Integer i = 0;
-        String text ="";
-        while (i< tResults.size()){
-
-            text += tResults.get(i).id +" ";
-            text += tResults.get(i).teksti +" ";
-            text += tResults.get(i).pvm;
-            text += "\n";
-            i++;
-
-
-        }
-        textViewTulos.setText(text);
-
-
+        ShowResults();
     }
 
+public void ShowResults(){
+    List<Taulu> tResults=tauluDao.getAllInDescenfingOrder();
+    adapteri = new Adapteri(this,R.layout.adapteri, tResults); //Mitähän tuohon viimeseen piti laittaa
+    listView.setAdapter(adapteri);
+    adapteri.addAll(tResults);
+    Integer i = 0;
+    String text ="";
+    while (i< tResults.size()){
 
+        text += tResults.get(i).id +" ";
+        text += tResults.get(i).teksti +" ";
+        text += tResults.get(i).pvm;
+        text += "\n";
+        i++;
+
+    }
+    textViewTulos.setText(text);
 }
-/*
 
-class SingleRow {
-    String Otsikko;
-    String Teksti;
-
-    SingleRow(String otsikko, String teksti) {
-        this.Teksti = teksti;
-        this.Otsikko = otsikko;
-    }
 }
 
 
 
-class CustomAdapter extends BaseAdapter {
-
-    ArrayList<SingleRow> list;
-    Context c;
-
-    CustomAdapter(Context context) {
-        c = context;
-        list = new ArrayList<SingleRow>();
-        list.add(new SingleRow("Testiotsikko","TestiTeksti")); //TESTATAAN ENSN KIINTEILLÄ
-        list.add(new SingleRow("Testiotsikko2","TestiTeksti2")); //TESTATAAN ENSN KIINTEILLÄ
-    }
-
-
-    @Override
-    public int getCount() {
-        return list.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-
-        return list.get(position);
-        //return null;
-
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        LayoutInflater layoutInflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        //View row = layoutInflater.inflate(R.layout.omalayout, parent, false);
-        View row = layoutInflater.inflate(R.layout.activity_main,parent,false);
-        TextView otsikko = (TextView) row.findViewById(R.id.otsikko);
-        TextView teksti = (TextView) row.findViewById(R.id.textiosa);
-        SingleRow tmp = list.get(position);
-        otsikko.setText(tmp.Otsikko);
-        teksti.setText(tmp.Teksti);
-        return row;
-    }
-}
-
-
-*/
-
-
-
-/*
-public class adapteri extends ArrayAdapter<String>{
-
-    ArrayList<String> dataset;
-
-    public adapteri(@NonNull Context context, int resource, @NonNull List objects) {
-        super(context, resource, objects);
-
-        //this.context = context;
-        this.dataset = (ArrayList<String>)objects;
-
-
-
-    }
-
-    //TODO Tämä adapteri....
-
-    @Override
-    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-
-        LayoutInflater layoutInflater = LayoutInflater.from(context);
-        View v = layoutInflater.inflate(R.layout.omalayout,parent,false);
-        LinearLayout linearLayout = (LinearLayout) v;
-
-        TextView lista = v.findViewById(R.id.lista);
-        lista.setText(dataset.get(position).toString());
-
-        Button poista = v.findViewById(R.id.poista);
-
-        poista.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dataset.remove(position);
-                notifyDataSetChanged();
-
-            }
-        });
-
-
-        return v;
-    }
-
-}
-*/
